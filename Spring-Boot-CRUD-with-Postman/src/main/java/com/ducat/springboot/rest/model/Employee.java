6150ca596ab4ee4236577e4760648fdf268db517
Jayanthyuva7 <122073868+Jayanthyuva7@users.noreply.github.com>
2024-03-01 19:03:53 +0530

    Add files via upload

---
+++ Spring-Boot-CRUD-with-Postman/src/main/java/com/ducat/springboot/rest/model/Employee.java
@@ -0,0 +1,57 @@
+package com.ducat.springboot.rest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.stereotype.Component;
+
+@Component
+
+// Spring jpa jars.
+@Entity
+@Table(name= "employee")
+
+// To increase speed and save sql statement execution time.
+@DynamicInsert
+@DynamicUpdate
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String department;
+	
+
+	public Employee() { }
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", salary=" +  "]";
+	}
+}
